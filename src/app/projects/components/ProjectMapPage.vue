90eae565907d09a22871e3b316d5592ace696c3f
Rob Gaston <rgaston@fargeo.com>
2018-03-14 18:23:14 -0700

    refactoring, re: #8

--- src/app/projects/components/ProjectMapPage.vue
+++ src/app/projects/components/ProjectMapPage.vue
@@ -7,18 +7,17 @@ import uuidv4 from 'uuid/v4';
 import 'mapbox-gl-cordova-offline/www/mapbox-gl.css';
 import basemapLayers from '../../../assets/map/basemap_layers.json';
 const mapboxgl = window.mapboxgl;
-const staticPath = 'static/map';
-const osmAttr = '<a href="http://www.openmaptiles.org/" ' +
-    'target="_blank">&copy; OpenMapTiles</a> ' +
-    '<a href="http://www.openstreetmap.org/about/" target="_blank">' +
-    '&copy; OpenStreetMap contributors</a>';
+const staticPath = 'static/map/';
+const osmAttr = '<a href="http://www.openmaptiles.org/" target="_blank">' +
+    '&copy; OpenMapTiles</a> <a href="http://www.openstreetmap.org/about/" ' +
+    'target="_blank">&copy; OpenStreetMap contributors</a>';
 
 export default {
     name: 'ProjectMap',
     data() {
         return {
             mapId: `mapboxgl-map-${uuidv4()}`,
-            mbTilesPath: staticPath + '/offline_basemap_test.mbtiles',
+            mbtilesFile: 'offline_basemap_test.mbtiles',
             center: [-122.4194, 37.7749],
             zoom: 12
         };
@@ -37,12 +36,12 @@ export default {
                     sources: {
                         openmaptiles: {
                             type: 'mbtiles',
-                            path: this.mbTilesPath,
+                            path: `${staticPath + this.mbtilesFile}`,
                             attribution: osmAttr
                         }
                     },
-                    sprite: staticPath + '/styles/klokantech-basic/sprite',
-                    glyphs: staticPath + '/fonts/{fontstack}/{range}.pbf',
+                    sprite: `${staticPath}styles/klokantech-basic/sprite`,
+                    glyphs: `${staticPath}fonts/{fontstack}/{range}.pbf`,
                     layers: basemapLayers
                 },
                 hash: true
