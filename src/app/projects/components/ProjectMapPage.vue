3b8f2858d1136b3e9dba4dc399f715fc39e353ba
Rob Gaston <rgaston@fargeo.com>
2018-03-14 17:49:29 -0700

    cleanup, re: #8

--- src/app/projects/components/ProjectMapPage.vue
+++ src/app/projects/components/ProjectMapPage.vue
@@ -5,6 +5,7 @@
 <script>
 import uuidv4 from 'uuid/v4';
 import 'mapbox-gl-cordova-offline/www/mapbox-gl.css';
+import basemapLayers from '../../../assets/map/basemap_layers.json';
 const mapboxgl = window.mapboxgl;
 
 export default {
@@ -14,15 +15,34 @@ export default {
             mapId: `mapboxgl-map-${uuidv4()}`
         };
     },
-    props: {},
     mounted() {
         this.mapInit();
     },
     methods: {
         mapInit() {
+            const osmAttr = '<a href="http://www.openmaptiles.org/" ' +
+                'target="_blank">&copy; OpenMapTiles</a> ' +
+                '<a href="http://www.openstreetmap.org/about/" target="_blank">' +
+                '&copy; OpenStreetMap contributors</a>';
             new mapboxgl.OfflineMap({
                 container: this.mapId,
-                style: 'static/map/style-offline.json',
+                style: {
+                    'version': 8,
+                    'center': [-122.4194, 37.7749],
+                    'zoom': 12,
+                    'bearing': 0,
+                    'pitch': 0,
+                    'sources': {
+                        'openmaptiles': {
+                            'type': 'mbtiles',
+                            'path': 'static/map/offline_basemap_test.mbtiles',
+                            'attribution': osmAttr
+                        }
+                    },
+                    'sprite': 'static/map/styles/klokantech-basic/sprite',
+                    'glyphs': 'static/map/fonts/{fontstack}/{range}.pbf',
+                    'layers': basemapLayers
+                },
                 hash: true
             }).then((map) => {
                 map.addControl(new mapboxgl.NavigationControl());
