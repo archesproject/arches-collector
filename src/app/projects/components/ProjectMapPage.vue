4c2d3a6dce25dd5b8caecc267abd5f0a743ed11b
Rob Gaston <rgaston@fargeo.com>
2018-03-14 18:01:04 -0700

    refactoring, re: #38

--- src/app/projects/components/ProjectMapPage.vue
+++ src/app/projects/components/ProjectMapPage.vue
@@ -7,12 +7,20 @@ import uuidv4 from 'uuid/v4';
 import 'mapbox-gl-cordova-offline/www/mapbox-gl.css';
 import basemapLayers from '../../../assets/map/basemap_layers.json';
 const mapboxgl = window.mapboxgl;
+const staticPath = 'static/map';
+const osmAttr = '<a href="http://www.openmaptiles.org/" ' +
+    'target="_blank">&copy; OpenMapTiles</a> ' +
+    '<a href="http://www.openstreetmap.org/about/" target="_blank">' +
+    '&copy; OpenStreetMap contributors</a>';
 
 export default {
     name: 'ProjectMap',
     data() {
         return {
-            mapId: `mapboxgl-map-${uuidv4()}`
+            mapId: `mapboxgl-map-${uuidv4()}`,
+            mbTilesPath: staticPath + '/offline_basemap_test.mbtiles',
+            center: [-122.4194, 37.7749],
+            zoom: 12
         };
     },
     mounted() {
@@ -20,28 +28,22 @@ export default {
     },
     methods: {
         mapInit() {
-            const osmAttr = '<a href="http://www.openmaptiles.org/" ' +
-                'target="_blank">&copy; OpenMapTiles</a> ' +
-                '<a href="http://www.openstreetmap.org/about/" target="_blank">' +
-                '&copy; OpenStreetMap contributors</a>';
             new mapboxgl.OfflineMap({
                 container: this.mapId,
                 style: {
-                    'version': 8,
-                    'center': [-122.4194, 37.7749],
-                    'zoom': 12,
-                    'bearing': 0,
-                    'pitch': 0,
-                    'sources': {
-                        'openmaptiles': {
-                            'type': 'mbtiles',
-                            'path': 'static/map/offline_basemap_test.mbtiles',
-                            'attribution': osmAttr
+                    version: 8,
+                    center: this.center,
+                    zoom: this.zoom,
+                    sources: {
+                        openmaptiles: {
+                            type: 'mbtiles',
+                            path: this.mbTilesPath,
+                            attribution: osmAttr
                         }
                     },
-                    'sprite': 'static/map/styles/klokantech-basic/sprite',
-                    'glyphs': 'static/map/fonts/{fontstack}/{range}.pbf',
-                    'layers': basemapLayers
+                    sprite: staticPath + '/styles/klokantech-basic/sprite',
+                    glyphs: staticPath + '/fonts/{fontstack}/{range}.pbf',
+                    layers: basemapLayers
                 },
                 hash: true
             }).then((map) => {
