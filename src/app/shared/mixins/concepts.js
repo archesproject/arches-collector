72c47a5086e958e58aca75b2a4743d59666a285c
Cyrus Hiatt <chiatt@fargeo.com>
2018-11-27 13:58:31 -0800

    Prevents error in report when the concept widget's value is not yet defined. re #133

--- src/app/shared/mixins/concepts.js
+++ src/app/shared/mixins/concepts.js
@@ -9,19 +9,27 @@ export default {
         conceptLabels: {
             get: function() {
                 var self = this;
-                return self.value.map(function(val) {
-                    return self.options.find(function(option) {
-                        return option.valueid === val;
+                if (Array.isArray(self.value)) {
+                    return self.value.map(function(val) {
+                        return self.options.find(function(option) {
+                            return option.valueid === val;
+                        });
                     });
-                });
+                } else {
+                    return "";
+                }
             }
         },
         conceptLabel: {
             get: function() {
                 var self = this;
-                return this.options.find(function(val) {
-                    return val.valueid === self.value;
-                });
+                if (self.value) {
+                    return this.options.find(function(val) {
+                        return val.valueid === self.value;
+                    });
+                } else {
+                    return '';
+                }
             }
         }
     }
