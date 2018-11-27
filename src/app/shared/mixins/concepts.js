eceff5c404b3232fc00169d4c49e6adf5aee8e37
Cyrus Hiatt <chiatt@fargeo.com>
2018-11-27 10:21:58 -0800

    removes cruft

--- src/app/shared/mixins/concepts.js
+++ src/app/shared/mixins/concepts.js
@@ -9,23 +9,19 @@ export default {
         conceptLabels: {
             get: function() {
                 var self = this;
-                var res = self.value.map(function(val) {
+                return self.value.map(function(val) {
                     return self.options.find(function(option) {
                         return option.valueid === val;
                     });
                 });
-                console.log(res);
-                return res;
             }
         },
         conceptLabel: {
             get: function() {
                 var self = this;
-                var res = this.options.find(function(val) {
+                return this.options.find(function(val) {
                     return val.valueid === self.value;
                 });
-                console.log(res);
-                return res;
             }
         }
     }
