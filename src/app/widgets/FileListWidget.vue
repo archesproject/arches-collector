36923da1594f7b62477ccc69784325f7a19da5b5
Cyrus Hiatt <chiatt@fargeo.com>
2018-11-29 17:07:46 -0800

    Saves photos to album when taken and allows user to select photo from album. re #212

--- src/app/widgets/FileListWidget.vue
+++ src/app/widgets/FileListWidget.vue
@@ -1,7 +1,19 @@
 <template>
     <div v-if="context=='editor'">
-        <div class="editor widget-label">{{widget.label}}</div>
-        <div class="done-btn"><v-ons-button @click="openCamera"><v-ons-icon class="camera-icon resource-header" icon="ion-camera"></v-ons-icon></v-ons-button></div>
+        <div class="editor widget-label file-widget">{{widget.label}}</div>
+        <ons-row class="file-widget">
+            <ons-col v-if="!value">No images yet collected</ons-col>
+            <ons-col v-if="value">
+                <ons-row v-for="file in value.files">
+                    <ons-col>{{file.name}}</ons-col>
+                    <ons-col>{{file.type}}</ons-col>
+                </ons-row>
+            </ons-col>
+        </ons-row>
+        <ons-row class="file-widget">
+            <ons-col><v-ons-button @click="takePhoto"><v-ons-icon class="camera-icon" icon="ion-camera"></v-ons-icon><span class="btn-text">Take Photo</span></v-ons-button></ons-col>
+            <ons-col><v-ons-button @click="selectPhoto"><v-ons-icon class="folder-icon" icon="ion-folder"></v-ons-icon><span class="btn-text">Select Photo</span></v-ons-button></ons-col>
+        </ons-row>
     </div>
     <ons-row class="report-widget" v-else-if="context=='report'">
         <ons-col class="report widget-label">{{widget.label}}</ons-col>
@@ -17,52 +29,98 @@ export default {
     data() {
         return {};
     },
+    mounted() {
+        this.init()
+    },
     methods: {
+        init: function(){
+            console.log(this.value);
+        },
         updateList: function() {
             this.$emit('update:value', ret);
         },
-        openCamera: function() {
-            console.log("new attempt")
-            console.log(navigator.camera);
-            var srcType = Camera.PictureSourceType.CAMERA;
-            var options = this.setOptions(srcType);
-            // var func = createNewFileEntry;
+        takePhoto: function() {
+            var options = this.setOptions(Camera.PictureSourceType.CAMERA, true);
+            this.getPhoto(options);
+        },
+        selectPhoto: function() {
+            var options = this.setOptions(Camera.PictureSourceType.PHOTOLIBRARY, false);
+            this.getPhoto(options);
+        },
+        getPhoto: function(options) {
+            var self = this;
             navigator.camera.getPicture(function cameraSuccess(imageUri) {
-                displayImage(imageUri);
-                func(imageUri);
+                console.log('here we do something with our file', imageUri);
+                self.attachImage(imageUri);
+                navigator.camera.cleanup(self.onCleanupSuccess, self.onCleanupFail);
             }, function cameraError(error) {
                 console.debug("Unable to obtain picture: " + error, "app");
             }, options);
         },
-        displayImage(imgUri) {
-            var elem = document.getElementById('imageFile');
-            elem.src = imgUri;
+        attachImage: function(imgUri) {
+            var image = {
+                name: "some photo.jpg",
+                type: "image/jpeg"
+            };
+            if (this.value) {
+                this.value.files.push(image);
+            } else {
+                this.value = {files:[image]}
+            }
+            console.log(JSON.parse(JSON.stringify(this.value)));
+            console.log(this.tile);
         },
-        setOptions(srcType) {
+        setOptions(srcType, saveToAlbum) {
             var options = {
-                // Some common settings are 20, 50, and 100
                 quality: 50,
-                destinationType: Camera.DestinationType.FILE_URI,
-                // In this app, dynamically set the picture source, Camera or photo gallery
+                destinationType: Camera.DestinationType.DATA_URL,
                 sourceType: srcType,
                 encodingType: Camera.EncodingType.JPEG,
                 mediaType: Camera.MediaType.PICTURE,
-                allowEdit: true,
-                correctOrientation: true  //Corrects Android orientation quirks
+                saveToPhotoAlbum: saveToAlbum,
+                allowEdit: false,
+                correctOrientation: true
             }
             return options;
         },
-        onSuccess() {
+        onCleanupSuccess() {
             console.log("Camera cleanup success.")
         },
-        onFail() {
+        onCleanupFail(message) {
             console.log('Failed because: ' + message);
         }
     },
     computed: {
     }
 };
+/*
+{
+    "41d064e3-f367-11e8-b0d1-c4b301baab9f": [
+        {
+            "url": "/files/uploadedfiles/Photo_on_3-16-18_at_11.49_AM_2_keJCgpc.jpg",
+            "name": "Photo on 3-16-18 at 11.49 AM #2.jpg",
+            "size": 168641,
+            "type": "image/jpeg",
+            "index": 0,
+            "width": 1080,
+            "height": 720,
+            "status": "uploaded",
+            "content": "blob:http://localhost:8000/96d93563-4953-4f83-aa1a-4a3c24f08ba9",
+            "file_id": "309989c7-f420-11e8-9771-c4b301baab9f",
+            "accepted": true,
+            "lastModified": 1521226186044
+        }
+    ]
+}
+*/
 </script>
 
 <style scoped>
+.file-widget {
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+.btn-text {
+    padding-left: 5px;
+}
 </style>
