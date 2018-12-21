d02f02ed92af0a759b37fa606e78d63f8e6fd2a6
Rob Gaston <rgaston@fargeo.com>
2018-12-21 12:07:38 -0800

    initial working map widget, re: #160

--- src/app/widgets/GeojsonFeatureCollectionWidget.vue
+++ src/app/widgets/GeojsonFeatureCollectionWidget.vue
@@ -2,7 +2,7 @@
     <div v-if="context=='editor'">
         <div class="editor widget-label">{{widget.label}}</div>
         <div class="map-wrapper">
-            <project-map></project-map>
+            <project-map v-on:map-init="mapInit"></project-map>
         </div>
     </div>
     <ons-row class="report-widget" v-else-if="context=='report'">
@@ -13,6 +13,9 @@
 
 
 <script>
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+
 export default {
     name: 'GeojsonFeatureCollectionWidget',
     props: ['value', 'widget', 'context'],
@@ -20,14 +23,40 @@ export default {
         return {};
     },
     methods: {
-    },
-    computed: {
+        mapInit: function(map) {
+            var self = this;
+            this.map = map;
+            this.draw = new MapboxDraw({
+                controls: {
+                    'combine_features': false,
+                    'uncombine_features': false
+                }
+            });
+            this.map.addControl(this.draw, 'top-left');
+            if (typeof this.value === 'object') {
+                this.draw.add(this.value);
+            }
+            map.on('draw.create', function() {
+                self.updateValue();
+            });
+            map.on('draw.update', function() {
+                self.updateValue();
+            });
+            map.on('draw.delete', function() {
+                self.updateValue();
+            });
+        },
+        updateValue: function() {
+            console.log(this.draw);
+            this.value = this.draw.getAll();
+            this.$emit('update:value', this.value);
+        }
     }
 };
 </script>
 
 <style scoped>
     .map-wrapper {
-        height: 200px;
+        height: 260px;
     }
 </style>
