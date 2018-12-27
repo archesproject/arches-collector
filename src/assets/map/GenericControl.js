13f13b526fa2b06654d26119e1d558df8596a09c
Rob Gaston <rgaston@fargeo.com>
2018-12-27 10:11:54 -0800

    factors out map widget layers and controls re:#160

---
+++ src/assets/map/GenericControl.js
@@ -0,0 +1,13 @@
+class GenericControl {
+    constructor(container) {
+        this._container = container;
+    }
+    onAdd() {
+        return this._container;
+    }
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+    }
+}
+
+export default GenericControl;
