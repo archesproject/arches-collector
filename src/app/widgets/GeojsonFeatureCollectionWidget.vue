30d377235c02c34234bc24efb1513106a93e9cc2
Rob Gaston <rgaston@fargeo.com>
2018-12-27 08:35:50 -0800

    syncs report with form, re: #160

--- src/app/widgets/GeojsonFeatureCollectionWidget.vue
+++ src/app/widgets/GeojsonFeatureCollectionWidget.vue
@@ -2,20 +2,30 @@
     <div>
         <div class="fullscreen-control">
             <div class="mapboxgl-ctrl mapboxgl-ctrl-group">
-                <button class="mapboxgl-ctrl-icon mapboxgl-ctrl-fullscreen" type="button" v-on:click="toggleFullscreen"></button>
+                <button class="mapboxgl-ctrl-icon" type="button"
+                    v-on:click="toggleFullscreen" v-bind:class="{
+                        'mapboxgl-ctrl-shrink': fullscreenActive,
+                        'mapboxgl-ctrl-fullscreen': !fullscreenActive
+                }"></button>
             </div>
         </div>
         <div v-if="context=='editor'">
             <div class="editor widget-label">{{widget.label}}</div>
-            <div class="map-wrapper" v-bind:class="{fullscreen: fullscreenActive}">
-                <project-map v-on:map-init="mapInit" :extent="bounds"></project-map>
+            <div class="map-wrapper" v-bind:class="{
+                fullscreen: fullscreenActive
+            }">
+                <project-map v-on:map-init="mapInit" :extent="bounds">
+                </project-map>
             </div>
         </div>
         <div class="report-widget" v-else-if="context=='report'">
             <ons-col class="report widget-label">{{widget.label}}</ons-col>
             <ons-col class="report widget-value">
-                <div class="map-wrapper" v-bind:class="{fullscreen: fullscreenActive}">
-                    <project-map v-on:map-init="mapInit" :extent="bounds"></project-map>
+                <div class="map-wrapper" v-bind:class="{
+                    fullscreen: fullscreenActive
+                }">
+                    <project-map v-on:map-init="mapInit" :extent="bounds">
+                    </project-map>
                 </div>
             </ons-col>
         </div>
@@ -60,13 +70,27 @@ export default {
             return fc.features.length > 0 ? fc : bounds;
         }
     },
+    watch: {
+        value: function(value) {
+            this.featureCollection = typeof value === 'object' ? value : {
+                type: 'FeatureCollection',
+                features: []
+            };
+            if (this.context === 'editor') {
+                this.draw.set(this.featureCollection);
+            } else {
+                let source = this.map.getSource('report-data');
+                source.setData(this.featureCollection);
+            }
+        }
+    },
     methods: {
         mapInit(map) {
             this.map = map;
             map.addControl(new FullscreenControl(
                 this.$el.querySelector('.fullscreen-control div')
             ));
-            if (this.context=='editor') {
+            if (this.context === 'editor') {
                 this.draw = new MapboxDraw({
                     controls: {
                         'combine_features': false,
@@ -75,7 +99,9 @@ export default {
                 });
                 this.map.addControl(this.draw, 'top-left');
                 this.draw.add(this.featureCollection);
-                map.on('draw.render', () => this.updateValue());
+                map.on('draw.render', () => {
+                    this.$emit('update:value', this.draw.getAll());
+                });
             } else {
                 const color = '#3bb2d0';
                 let style = map.getStyle();
@@ -90,7 +116,8 @@ export default {
                     'paint': {
                         'fill-color': color,
                         'fill-opacity': 0.1
-                    }
+                    },
+                    "filter": ["==", "$type", "Polygon"]
                 }, {
                     'id': 'report-line',
                     'type': 'line',
@@ -113,10 +140,6 @@ export default {
                 });
                 map.setStyle(style);
             }
-        },
-        updateValue() {
-            this.featureCollection = this.draw.getAll();
-            this.$emit('update:value', this.featureCollection);
         },
         toggleFullscreen() {
             this.fullscreenActive = !this.fullscreenActive;
