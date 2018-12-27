e839ccb9b3b547c32664d27c8d91e2ceb7a9a0c5
Rob Gaston <rgaston@fargeo.com>
2018-12-27 09:46:56 -0800

    refactors and cleans, re: #160

--- src/app/widgets/GeojsonFeatureCollectionWidget.vue
+++ src/app/widgets/GeojsonFeatureCollectionWidget.vue
@@ -1,43 +1,33 @@
 <template>
-    <div>
-        <div class="fullscreen-control">
-            <div class="mapboxgl-ctrl mapboxgl-ctrl-group">
-                <button class="mapboxgl-ctrl-icon" type="button"
-                    v-on:click="toggleFullscreen" v-bind:class="{
+<div>
+    <div class="fullscreen-control">
+        <div class="mapboxgl-ctrl mapboxgl-ctrl-group">
+            <button class="mapboxgl-ctrl-icon" type="button" v-on:click="toggleFullscreen" v-bind:class="{
                         'mapboxgl-ctrl-shrink': fullscreenActive,
                         'mapboxgl-ctrl-fullscreen': !fullscreenActive
                 }"></button>
-            </div>
-        </div>
-        <div v-if="context=='editor'">
-            <div class="editor widget-label">{{widget.label}}</div>
-            <div class="map-wrapper" v-bind:class="{
-                fullscreen: fullscreenActive
-            }">
-                <project-map v-on:map-init="mapInit" :extent="bounds">
-                </project-map>
-            </div>
-        </div>
-        <div class="report-widget" v-else-if="context=='report'">
-            <ons-col class="report widget-label">{{widget.label}}</ons-col>
-            <ons-col class="report widget-value">
-                <div class="map-wrapper" v-bind:class="{
-                    fullscreen: fullscreenActive
-                }">
-                    <project-map v-on:map-init="mapInit" :extent="bounds">
-                    </project-map>
-                </div>
-            </ons-col>
         </div>
     </div>
+    <div v-if="context=='editor'">
+        <div class="editor widget-label">{{widget.label}}</div>
+        <div class="map-wrapper" v-bind:class="{ fullscreen: fullscreenActive }">
+            <project-map v-on:map-init="mapInit" :extent="bounds"></project-map>
+        </div>
+    </div>
+    <div class="report-widget" v-else-if="context=='report'">
+        <ons-col class="report widget-label">{{widget.label}}</ons-col>
+        <ons-col class="report widget-value">
+            <div class="map-wrapper" v-bind:class="{ fullscreen: fullscreenActive }">
+                <project-map v-on:map-init="mapInit" :extent="bounds"></project-map>
+            </div>
+        </ons-col>
+    </div>
+</div>
 </template>
-
 
 <script>
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
-
-const mapboxgl = window.mapboxgl;
 
 class FullscreenControl {
     constructor(container) {
@@ -51,11 +41,44 @@ class FullscreenControl {
     }
 }
 
+const color = '#3bb2d0';
+const reportLayers = [{
+    'id': 'report-fill',
+    'type': 'fill',
+    'source': 'report-data',
+    'paint': {
+        'fill-color': color,
+        'fill-opacity': 0.1
+    },
+    "filter": ["==", "$type", "Polygon"]
+}, {
+    'id': 'report-line',
+    'type': 'line',
+    'source': 'report-data',
+    'paint': {
+        'line-color': color,
+        'line-width': 2
+    }
+}, {
+    'id': 'report-circle',
+    'type': 'circle',
+    'source': 'report-data',
+    'paint': {
+        'circle-color': color,
+        'circle-radius': 3,
+        'circle-stroke-width': 2,
+        'circle-stroke-color': 'white'
+    },
+    "filter": ["==", "$type", "Point"]
+}];
+
 export default {
     name: 'GeojsonFeatureCollectionWidget',
     props: ['value', 'widget', 'context'],
     data() {
-        return { fullscreenActive: false };
+        return {
+            fullscreenActive: false
+        };
     },
     computed: {
         featureCollection() {
@@ -65,9 +88,7 @@ export default {
             };
         },
         bounds() {
-            const bounds = this.$store.getters.activeProject.bounds;
-            const fc = this.featureCollection;
-            return fc.features.length > 0 ? fc : bounds;
+            return this.featureCollection.features.length > 0 ? this.featureCollection : bounds;
         }
     },
     watch: {
@@ -87,54 +108,28 @@ export default {
             map.addControl(new FullscreenControl(
                 this.$el.querySelector('.fullscreen-control div')
             ));
-            if (this.context === 'editor') {
-                this.draw = new MapboxDraw({
-                    controls: {
-                        'combine_features': false,
-                        'uncombine_features': false
-                    }
-                });
-                this.map.addControl(this.draw, 'top-left');
-                this.draw.add(this.featureCollection);
-                map.on('draw.render', () => this.updateDrawings());
-            } else {
-                const color = '#3bb2d0';
-                let style = map.getStyle();
-                style.sources['report-data'] = {
-                    'type': 'geojson',
-                    'data': this.featureCollection
-                };
-                style.layers.push({
-                    'id': 'report-fill',
-                    'type': 'fill',
-                    'source': 'report-data',
-                    'paint': {
-                        'fill-color': color,
-                        'fill-opacity': 0.1
-                    },
-                    "filter": ["==", "$type", "Polygon"]
-                }, {
-                    'id': 'report-line',
-                    'type': 'line',
-                    'source': 'report-data',
-                    'paint': {
-                        'line-color': color,
-                        'line-width': 2
-                    }
-                }, {
-                    'id': 'report-circle',
-                    'type': 'circle',
-                    'source': 'report-data',
-                    'paint': {
-                        'circle-color': color,
-                        'circle-radius': 3,
-                        'circle-stroke-width': 2,
-                        'circle-stroke-color': 'white'
-                    },
-                    "filter": ["==", "$type", "Point"]
-                });
-                map.setStyle(style);
-            }
+            if (this.context === 'editor') this.initDraw();
+            else this.initReport();
+        },
+        initDraw() {
+            this.draw = new MapboxDraw({
+                controls: {
+                    'combine_features': false,
+                    'uncombine_features': false
+                }
+            });
+            this.map.addControl(this.draw, 'top-left');
+            this.draw.add(this.featureCollection);
+            this.map.on('draw.render', () => this.updateDrawings());
+        },
+        initReport() {
+            let style = this.map.getStyle();
+            style.sources['report-data'] = {
+                'type': 'geojson',
+                'data': this.featureCollection
+            };
+            style.layers.push(...reportLayers);
+            this.map.setStyle(style);
         },
         updateDrawings() {
             this.$emit('update:value', this.draw.getAll());
@@ -151,24 +146,27 @@ export default {
 </script>
 
 <style scoped>
-    .map-wrapper {
-        height: 260px;
-    }
-    .report .map-wrapper {
-        margin-top: 5px;
-        margin-right: 20px;
-    }
-    .fullscreen.map-wrapper {
-        height: auto;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        left: 0;
-        z-index: 10;
-        margin: 0;
-    }
-    .fullscreen-control {
-        display: none;
-    }
+.map-wrapper {
+    height: 260px;
+}
+
+.report .map-wrapper {
+    margin-top: 5px;
+    margin-right: 20px;
+}
+
+.fullscreen.map-wrapper {
+    height: auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 10;
+    margin: 0;
+}
+
+.fullscreen-control {
+    display: none;
+}
 </style>
