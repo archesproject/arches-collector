2432a1c60facc8f5a774f2bf27c261efb32c49f6
njkim <nkim@fargeo.com>
2020-07-24 11:48:02 -0700

    Change the name field  of the v4 datewidget

--- src/app/widgets/DateWidget-v4.vue
+++ src/app/widgets/DateWidget-v4.vue
@@ -19,7 +19,7 @@
 <script>
 import moment from 'moment';
 export default {
-    name: 'DateWidget',
+    name: 'DateWidget-v4',
     props: ['value', 'widget', 'context'],
     data() {
         return {};
